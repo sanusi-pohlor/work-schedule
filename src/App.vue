f2c65c0d97e5eee7029971a586bfdc6a11b247fd
sanusi-pohlor <134247007+sanusi-pohlor@users.noreply.github.com>
2025-08-03 23:37:38 +0700

    add navbar sidebar card status

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
 <script setup>
-import Sidebar from './components/Sidebar.vue';
-import Navbar from './components/Navbar.vue';
+
 </script>
 
 <template>
-  <div class="flex flex-col min-h-screen">
-    <Navbar />
-    <div class="flex flex-1">
-      <Sidebar />
-      <main class="flex-1 p-4 overflow-y-auto">
-        <router-view />
-      </main>
-    </div>
+  <div>
+    ฟอร์มนี้ใช้สำหรับการทดสอบการแสดงผลของ Vue.js
   </div>
 </template>
-
-<style scoped>
-</style>
